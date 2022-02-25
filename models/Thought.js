e8798178f5cd65c6147076ca42d711f63f00ec06
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-02-24 22:33:39 -0800

    add Thought model and fix thoughts and friends arrays in User model and delete thought function that wasn't needed

---
+++ models/Thought.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String, 
+      required: true,
+      minlength: 1, 
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date, 
+      default: Date.now,
+      
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'reaction',
+      },
+    ],
+  },
+  {
+    toJSON: {
+    virtuals: true,
+    },
+  }
+);
+
+ // Create a virtual property `getReactions` that gets the number of reactions associated with a thought
+thoughtSchema.virtual('getReactions').get(function () {
+  return this.reactions.length;
+});
+
+// Initialize Thought model
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,42 +1,42 @@
- const { Schema, model } = require('mongoose');
-import { isEmail } from 'validator'
+const { Schema, model } = require('mongoose');
+const { isEmail } =require('validator');
  // Schema to create User model
- const userSchema = new Schema(
-   {
-     username: {
-       type: String, 
-       unique: true,
-       required: true,
-       trim: true,
-     },
-     email: {
-       type: String, 
-       required: true,
-       unique: true,
-       validate: [ isEmail, 'invalid email']
-     },
-     thoughts: {
-       type: Schema.Types.ObjectId, 
-       ref: 'Thought',
-     },
-     friends: {
+const userSchema = new Schema(
+  {
+    username: {
+      type: String, 
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String, 
+      required: true,
+      unique: true,
+      validate: [ isEmail, 'invalid email']
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId, 
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
       type: Schema.Types.ObjectId, 
       ref: 'User',
-     },
-   },
-    {
+      },
+    ],
+  },
+  {
     toJSON: {
-      virtuals: true,
+    virtuals: true,
     },
   }
- );
+);
 
- // Create a virtual property `getthoughts` that gets the amount of thoughts associated with a user
-userSchema.virtual('getThoughts').get(function () {
-  return this.thoughts;
-});
-
-userSchema.virtual('getFriends').get(function () {
+ // Create a virtual property `getFriends` that gets the number of friends associated with a user
+ userSchema.virtual('getFriends').get(function () {
   return this.friends.length;
 });
 
